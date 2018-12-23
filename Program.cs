b464ef1f4ccf71922994c6894568aa5d6bdb6bc0
camilo <chc201627@gmail.com>
2018-12-22 21:23:46 -0500

    Creando metodo timbrar

--- Program.cs
+++ Program.cs
@@ -2,11 +2,33 @@
 
 namespace CoreEscuela
 {
+
+    class CoreEscuela
+    {
+        public string nombre;
+        public string direccion;
+        public int añoFundacion;
+
+        public string ceo;
+
+        public void Timbrar()
+        {
+            //Todo
+            Console.Beep(2000,3000);
+        }
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var  miEscuela = new CoreEscuela();
+            miEscuela.nombre = "Platzi Academy";
+            miEscuela.direccion = "Carrera 5 # 34-5";
+            miEscuela.añoFundacion  = 2004;
+            Console.WriteLine("TIMBRE");
+            miEscuela.Timbrar();
+            //Console.WriteLine("Hello World!");
         }
     }
 }
