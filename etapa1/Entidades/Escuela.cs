1aac46bf34f71d3467d6e50c86119e2f0a452aaa
camilo <chc201627@gmail.com>
2018-12-23 08:21:54 -0500

    Creando clase Escuela

---
+++ etapa1/Entidades/Escuela.cs
@@ -0,0 +1,26 @@
+namespace CoreEscuela.Entidades
+{
+    class Escuela
+    {
+        string nombre;
+        
+        public string Nombre{
+            get
+            { 
+                return "Copia : " + nombre;
+                
+            }
+            set
+            {
+                nombre = value.ToUpper();
+            }
+            
+        }
+        public int AñoDeCreacion{get;set;}
+        public string Pais { get; set; }
+        public string Ciudad { get; set; }
+
+    
+        public Escuela(string nombre, int año) => (Nombre,AñoDeCreacion) = (nombre,año);
+    }
+}
---
+++ etapa1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using CoreEscuela.Entidades;
+
+namespace etapa1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var escuela = new Escuela("Platzi Academy", 2012);
+            escuela.Pais="Colombia";
+            escuela.Ciudad = "Bogota";
+            Console.WriteLine(escuela.Nombre);
+        }
+    }
+}
