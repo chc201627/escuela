be3951a65e596ec4193863886726f196b4bfd1dc
camilo <chc201627@gmail.com>
2018-12-24 09:49:45 -0500

    Agregando arreglos

---
+++ etapa1/Entidades/Curso.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace CoreEscuela.Entidades
+{
+    public class Curso
+    {
+        public string Nombre{ get; set; }
+        public string UniqueId { get; private set; }
+        public TiposJornada Jornada { get; set; }
+        public Curso ()=> UniqueId = Guid.NewGuid().ToString();
+        
+    }
+}
--- etapa1/Program.cs
+++ etapa1/Program.cs
@@ -8,8 +8,40 @@ namespace etapa1
         static void Main(string[] args)
         {
             var escuela = new Escuela("Platzi Academy", 2012,TiposEscuela.PresEscolar,
-                        ciudad:"Bogota",pais:"Colombia");            
+                        ciudad:"Bogota",pais:"Colombia"); 
+
+            
+            var arregloCursos = new Curso[3];
+            
+            arregloCursos[0] = new Curso(){
+                Nombre = "101"
+
+            }; 
+            var curso2 = new Curso(){
+                Nombre = "201"
+
+            };
+            arregloCursos[1]=curso2;
+
+           arregloCursos[2] = new Curso(){
+                             Nombre = "301"
+
+                            };                
             Console.WriteLine(escuela);
+            System.Console.WriteLine("===============");
+            imprimiCursos(arregloCursos);
+          
+           
+        }
+
+        private static void imprimiCursos(Curso[] arregloCursos)
+        {
+            int contador=0;
+            while (contador<arregloCursos.Length)
+            {
+                Console.WriteLine($"Nombre : {arregloCursos[contador].Nombre}, Id{arregloCursos[contador].UniqueId}");
+                contador++;
+            }
         }
     }
 }
